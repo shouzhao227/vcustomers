bdddda35deab31a7f4f13ebcc38e5ce7c1a07b45
herrywu <aaa@163.com>
2018-04-10 22:29:16 +0800

    vcutomers

---
+++ src/components/About.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about container">
+	   <h1 class="page-header">关于我们</h1>
+	   <p>这是一个使用vue搭建的后台管理的项目！</p>
+	   <article>
+	   		<p>使用了vue-router，vue-resource</p>
+	   		<p>有增删改查四个常用功能</p>
+	   		<p>使用的本地接口json-server</p>
+	   </article>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/CustomerDetails.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="details container">
+   	<router-link to="/" class="btn btn-default">返回</router-link>	
+  	<h1 class="page-header">
+  		{{customer.name}}
+  		<span class="pull-right">
+  			<router-link class="btn btn-primary" v-bind:to="'/edit/'+customer.id">
+  				编辑
+  			</router-link>
+  			<button class="btn btn-danger" v-on:click="deleteCustomer(customer.id)">
+  				删除
+  		</button>
+  		</span>
+  	</h1>
+    <ul class="list-group">
+    	<li class="list-group-item">
+    		<span class="glyphicon glyphicon-phone">
+    			{{customer.phone}}
+    		</span>
+    	</li>
+    	<li class="list-group-item">
+    		<span class="glyphicon glyphicon-envelope">
+    			{{customer.email}}
+    		</span>
+    	</li>
+    </ul>
+
+    <ul class="list-group">
+    	<li class="list-group-item">
+    		<span class="glyphicon glyphicon-globe">
+    		{{customer.education}}
+    		</span>
+    	</li>
+    	<li class="list-group-item">
+    		<span class="glyphicon glyphicon-sunglasses">
+    		{{customer.graduationschool}}
+    		</span>
+    	</li>
+    	<li class="list-group-item">
+    		<span class="glyphicon glyphicon-asterisk">
+    		{{customer.profession}}
+    		</span>
+    	</li>
+    	<li class="list-group-item">
+    		<span class="glyphicon glyphicon-plus">
+    		{{customer.profile}}
+    		</span>
+    	</li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'customerdetails',
+  data () {
+    return {
+    	customer:""
+    }
+  },
+  methods:{
+  	fetchCustomers(id){
+      this.$http.get("http://localhost:3000/users/"+id)
+      .then(function(response){
+          this.customer=response.body;
+      })
+    },
+    deleteCustomer(id){
+		this.$http.delete("http://localhost:3000/users/"+id)
+		.then(function(response){
+				this.$router.push({path:"/",query:{alert:"用户删除成功！"}})
+		})
+    }
+  },
+  created(){
+  	this.fetchCustomers(this.$route.params.id);
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
